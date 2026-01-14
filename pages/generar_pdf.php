620c9bce95b27412911b8ec763f4bb1692fd5309
Luis <luis@MacBook-Pro-de-Luis.local>
2026-01-14 12:09:20 -0300

    logo remesa2

--- pages/generar_pdf.php
+++ pages/generar_pdf.php
@@ -140,10 +140,10 @@ $html = '
 
     <!-- === SECCIÓN SUPERIOR (CABECERA + 4x4) === -->
     <div class="section-box">
-        <!-- LOGO SUPERIOR IZQUIERDO -->
+       <!-- LOGO SUPERIOR IZQUIERDO -->
         <div class="logo-box">
-            <img src="/includes/LogoLG.jpeg" alt="Logo LG">
-        </div>
+            <img src="../includes/LogoLG.jpeg" alt="Logo LG">
+        </div>      
         <div style="margin-bottom: 10px;">
             <div style="font-weight: bold; font-size: 13px; margin-bottom: 6px; text-align: left;">Agencia de Aduanas Luis Galleguillos Valderrama</div>
             <h2 style="margin: 8px 0;">SOLICITUD DE REMESA</h2>
