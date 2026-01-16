03d6c20167b182e52c29f79abe75f270e0e00b4f
Luis <luis@MacBook-Pro-de-Luis.local>
2026-01-16 09:02:18 -0300

    login 220px

--- login.php
+++ login.php
@@ -31,7 +31,7 @@
             to { opacity: 1; transform: translateY(0); }
         }
         .login-logo {
-            width: 180px;
+            width: 220px;
             margin-bottom: 1rem;
             border-radius: 12px;
         }
